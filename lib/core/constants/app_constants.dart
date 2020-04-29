31d4aea7b03bcde91c04224202ebf5e5816907dd
giosave <giosave98@gmail.com>
2020-04-29 16:25:02 -0700

    Create widgets utils

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+// Dateformats
+const String main_date_format = 'yyyy-MM-dd';
+// Font Sizes
+const double FONT_SIZE_EXTRA_BIG = 26.0;
+const double FONT_SIZE_BIG = 23.0;
+const double FONT_SIZE_NORMAL = 18.0;
+const double FONT_SIZE_SMALL = 16.0;
+const double FONT_SIZE_EXTRA_SMALL = 14.0;
+// COLORS
+const PRIMARY_COLOR = Color.fromRGBO(33, 150, 243, 1.0);
+const DEFAULT_TEXT_COLOR = Color.fromRGBO(33, 33, 33, 1.0);
--- lib/ui/shared/widgets_utils.dart
+++ lib/ui/shared/widgets_utils.dart
@@ -1,1 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_tec_demo/core/constants/app_constants.dart';
+import 'package:quiz_tec_demo/ui/shared/ui_helpers.dart';
+
+enum FSize { ExtraBig, Big, Normal, Small, ExtraSmall }
+enum FWeight { Bold, Regular }
+
+double defineFontSize(FSize size) {
+  switch(size) {
+    case FSize.ExtraBig:
+      return FONT_SIZE_EXTRA_BIG;
+    case FSize.Big:
+      return FONT_SIZE_BIG;
+    case FSize.Normal:
+      return FONT_SIZE_NORMAL;
+    case FSize.Small:
+      return FONT_SIZE_SMALL;
+    case FSize.ExtraSmall:
+      return FONT_SIZE_EXTRA_SMALL;
+  }
+  return FONT_SIZE_NORMAL;
+}
+
+FontWeight defineFontWeight(FWeight weight) {
+  switch(weight) {
+    case FWeight.Bold:
+      return FontWeight.bold;
+    case FWeight.Regular:
+      return FontWeight.normal;
+  }
+  return FontWeight.normal;
+}
+
+Widget label(String text, [FSize size, FWeight weight, Color color = DEFAULT_TEXT_COLOR]) {
+  return Text(
+    text ?? '',
+    style: TextStyle(
+      color: color,
+      fontSize: defineFontSize(size),
+      fontWeight: defineFontWeight(weight),
+      fontFamily: 'SourceSans'
+    )
+  );
+}
+
+Widget buildColumn(String val, String heading) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      label(heading, FSize.Small, FWeight.Regular, PRIMARY_COLOR),
+      UIHelper.verticalSpaceSmall,
+      label(val ?? '', FSize.Small, FWeight.Bold),
+      UIHelper.verticalSpaceSmall
+    ]
+  );
+}
+
+Widget buildRow(String heading, String value, [FSize size = FSize.Normal, FWeight weight = FWeight.Regular, Color color = DEFAULT_TEXT_COLOR, MainAxisAlignment mainAxisAlignment = MainAxisAlignment.spaceBetween]) {
+  return Row(
+    mainAxisAlignment: mainAxisAlignment,
+    children: <Widget>[
+      label(heading ?? '', size, weight, color),
+      label(value ?? '', size, weight, color)
+    ]
+  );
+}
+
+Widget buildWrap(String heading, String value, [FSize size = FSize.Normal, FWeight weight = FWeight.Regular, Color color = DEFAULT_TEXT_COLOR]) {
+  return Wrap(
+    direction: Axis.horizontal,
+    alignment: WrapAlignment.spaceBetween,
+    children: <Widget>[
+      label(heading ?? '', size, weight, color),
+      label(value ?? '', size, weight, color)
+    ]
+  );
+}
+
+Widget buildContainer({Widget child, Color color}) {
+  return Container(
+    color: color,
+    alignment: Alignment.center,
+    margin: EdgeInsets.all(8),
+    child: child
+  );
+}
+
+Widget buildCard({Widget child, Color color}) {
+  return Card(
+    color: color,
+    elevation: 5,
+    child: child
+  );
+}
+
+Widget buildMessageNoData() {
+  return Container(
+    child: Center(
+      child: label("MENSAJE DE ERROR")
+    ),
+  );
+}
+
+Widget buildDropdownItemWithDivider({String text}) {
+  return Wrap(
+    alignment: WrapAlignment.start,
+    children: <Widget>[
+      label(text),
+      Divider(
+        color: Colors.black
+      )
+    ],
+  );
+}
