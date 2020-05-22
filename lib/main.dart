6aebc05992c84736eb01cf4427e41e216305bbdf
giosave <giosave98@gmail.com>
2020-05-22 13:03:43 -0700

    Add route for new screen splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_tec_demo/core/constants/app_route_constants.dart';
 
 import 'package:quiz_tec_demo/ui/views/question_home.dart';
+import 'package:quiz_tec_demo/ui/views/splash_screen_view.dart';
+import 'package:quiz_tec_demo/ui/widgets/router.dart';
 
 void main(){
   runApp(
       MaterialApp(
-        home: QuestionHome(),
+        home: SplashScreenView(),
+        //initialRoute: RoutePaths.Home,
+        title: 'fLUTTER DEMO',
+        onGenerateRoute: Router.generateRoute,
       )
   );
 }
