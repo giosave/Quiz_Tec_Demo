c6a2f15155a4745d49f1cbce1a51b8d7c32bfa35
giosave <giosave98@gmail.com>
2020-05-22 13:05:20 -0700

    Name description for route arguments

---
+++ lib/core/constants/app_route_constants.dart
@@ -0,0 +1,6 @@
+class RoutePaths {
+  static const String Home = 'home';
+  static const String Question ='question';
+  static const String QuestionResult = 'question_result';
+  static const String Splas = 'splash';
+}
