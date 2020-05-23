4c0933fcb67f932b7c753d9483c088ce28ec51a0
giosave <giosave98@gmail.com>
2020-05-22 17:29:26 -0700

    Change name for variable page home

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -12,7 +12,7 @@ const double FONT_SIZE_EXTRA_SMALL = 14.0;
 const PRIMARY_COLOR = Color.fromRGBO(33, 150, 243, 1.0);
 const DEFAULT_TEXT_COLOR = Color.fromRGBO(33, 33, 33, 1.0);
 //PAGES
-const String PAGE_HOME = "Aplicacion Inicio";
+const String PAGE_HOME = "Inicio";
 const String PAGE_QUESTIONNAIRE = 'Cuestionario';
 //PROFILE
 const PROFILE_ADMIN = 'Administrador';
