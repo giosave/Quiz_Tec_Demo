ffa9f35a1344e5504fa6aeacafcc307ae0f7fd57
giosave <giosave98@gmail.com>
2020-06-10 00:01:23 -0700

    Add fake data for web browser

--- lib/ui/views/information_view.dart
+++ lib/ui/views/information_view.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_tec_demo/ui/shared/widgets_utils.dart';
+import 'package:quiz_tec_demo/ui/widgets/fake_data.dart';
 
 class InformationView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return buildContainer(
-      child:label("Informacion")
+      child: ListView.builder(
+        itemCount: information.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                  title: Center(
+                    child: label(information[index]['titulo'], FSize.Big, FWeight.Bold)
+                  ),
+                  trailing: IconButton(
+                    iconSize: 30,
+                    focusColor: Colors.orange,
+                    color: Colors.lightGreen,
+                    icon: Icon(Icons.arrow_forward), 
+                    onPressed: (){})
+                ),
+              ],
+            ),
+          );
+        })
     );
   }
 }
