0ddfa3a3dd0100e1b33d6930863aab83f948d36d
giosave <giosave98@gmail.com>
2020-06-09 23:51:16 -0700

    Add new list and space

--- lib/ui/widgets/fake_data.dart
+++ lib/ui/widgets/fake_data.dart
@@ -1,47 +1,84 @@
 
   List<Map<String, dynamic>> dataContact = [{
-    'nombre': 'MEDICO ESCOLAR',
-    'direccion': 'EDIFICIO "M" MAT/VESP',
-    'numero': 6865804986,
-    'numero_externo': null,
-    'escuela': 'ITM',
-    'Horarios': null,
-    'correo': 'medicoescolar@itmexicali.edu.mx',
-  },
-  {
-    'nombre': 'Psicóloga Marisssa',
-    'direccion': 'Av. Misión de Zuni 376, Misión Santo Domingo, Mexicali, B.C.',
-    'numero': 6862016515,
-    'numero_externo': null,
-    'escuela': 'Externo',
-    'Horarios': null,
-    'correo': null
-  },
-  {
-    'nombre': 'CAPNAM',
-    'direccion': 'De La Marqueza 3299, Hacienda Dorada, 21376.Mexicali, B.C.',
-    'numero': 6868420206,
-    'numero_externo': null,
-    'escuela': 'Externo',
-    'Horarios': null,
-    'correo': null,
-  },
-  {
-    'nombre': 'Centro de Psiquiatría y Psicología Holokinética',
-    'direccion': 'Ave. Alba # 3679, Nuevo Amanecer, 21378.Mexicali, B.C.',
-    'numero': 6868420004,
-    'numero_externo': null,
-    'escuela': 'Externo',
-    'Horarios': null,
-    'correo': null,
-  },
-  {
-    'nombre': 'Centro de Atención Psicológica y Psiquiátrica',
-    'direccion': 'Ave. Argentina #279 Residencial  Hípico Plus.Mexicali, B.C.',
-    'numero': 6865548687,
-    'numero_externo': null,
-    'escuela': 'Externo',
-    'Horarios': null,
-    'correo': null,
-  },
+      'nombre': 'MEDICO ESCOLAR',
+      'direccion': 'EDIFICIO "M" MAT/VESP',
+      'numero': 6865804986,
+      'numero_externo': null,
+      'escuela': 'ITM',
+      'Horarios': null,
+      'correo': 'medicoescolar@itmexicali.edu.mx',
+    },
+    {
+      'nombre': 'Psicóloga Marisssa',
+      'direccion': 'Av. Misión de Zuni 376, Misión Santo Domingo, Mexicali, B.C.',
+      'numero': 6862016515,
+      'numero_externo': null,
+      'escuela': 'Externo',
+      'Horarios': null,
+      'correo': null
+    },
+    {
+      'nombre': 'CAPNAM',
+      'direccion': 'De La Marqueza 3299, Hacienda Dorada, 21376.Mexicali, B.C.',
+      'numero': 6868420206,
+      'numero_externo': null,
+      'escuela': 'Externo',
+      'Horarios': null,
+      'correo': null,
+    },
+    {
+      'nombre': 'Centro de Psiquiatría y Psicología Holokinética',
+      'direccion': 'Ave. Alba # 3679, Nuevo Amanecer, 21378.Mexicali, B.C.',
+      'numero': 6868420004,
+      'numero_externo': null,
+      'escuela': 'Externo',
+      'Horarios': null,
+      'correo': null,
+    },
+    {
+      'nombre': 'Centro de Atención Psicológica y Psiquiátrica',
+      'direccion': 'Ave. Argentina #279 Residencial  Hípico Plus.Mexicali, B.C.',
+      'numero': 6865548687,
+      'numero_externo': null,
+      'escuela': 'Externo',
+      'Horarios': null,
+      'correo': null,
+    },
+  ];
+
+  List<Map<String, dynamic>> information = [{
+      'titulo': 'Formas para controlar el estres.',
+      'contenido': 'TEXTO DE INTRODUCCION',
+      'link': 'https://kidshealth.org/es/teens/stress-tips-esp.html'
+    },
+    {
+      'titulo': 'Formas para controlar el estres.',
+      'contenido': 'TEXTO DE INTRODUCCION',
+      'link': 'https://www.healthline.com/health/es/como-quitar-el-estres'
+    },
+    {
+      'titulo': 'Formas para controlar el estres.',
+      'contenido': 'TEXTO DE INTRODUCCION',
+      'link': 'https://www.mayoclinic.org/es-es/healthy-lifestyle/stress-management/in-depth/stress-relievers/art-20047257'
+    },
+    {
+      'titulo': 'Estres laboral',
+      'contenido': 'TEXTO DE INTRODUCCION',
+      'link': 'https://www.salud.gob.sv/archivos/pdf/seguridad_ocupacional_2017_presentaciones/presentacion21092017/MANEJO-ADECUADO-DEL-ESTRES-LABORAL.pdf'
+    },
+    {
+      'titulo': 'Estres laboral',
+      'contenido': 'TEXTO DE INTRODUCCION',
+      'link': 'https://medlineplus.gov/spanish/ency/patientinstructions/000884.html'
+    },
+    {
+      'titulo': 'Estres academico.',
+      'contenido': 'TEXTO DE INTRODUCCION',
+      'link': 'https://www.uchile.cl/portal/presentacion/centro-de-aprendizaje-campus-sur/114600/reconocer-prevenir-y-afrontar-el-estres-academico'
+    },
+    {
+      'titulo': 'Estres academico',
+      'contenido': 'TEXTO DE INTRODUCCION',
+      'link': 'https://www.uv.es/iqdocent/guias/estres.pdf'
+    },
   ];
